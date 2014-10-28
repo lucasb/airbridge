4f0a4534498bf7f3935282dca8564bfa97cd58f8
lucasboscaini <lucasboscaini@gmail.com>
2014-10-28 02:33:50 -0200

    Add response json type and fix format code style.

--- airbridge/builder.py
+++ airbridge/builder.py
@@ -1,18 +1,18 @@
 # -*- config:utf-8 -*-
 """
-  Copyright 2014 Airbridge
+    Copyright 2014 Airbridge
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-      http://www.apache.org/licenses/LICENSE-2.0
+        http://www.apache.org/licenses/LICENSE-2.0
 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 """
 
 from flask import Flask
@@ -25,13 +25,13 @@ from airbridge.common.api import HttpError
 
 def create_app(module):
 
-  app = Flask('airbridge')
-  app.config.from_pyfile('config.cfg')
+    app = Flask('airbridge')
+    app.config.from_pyfile('config.cfg')
 
-  set_db(app)
-  HttpError(app)
+    set_db(app)
+    HttpError(app)
 
-  blueprint = import_string('airbridge.{0}.views.{0}'.format(module))
-  app.register_blueprint(blueprint)
+    blueprint = import_string('airbridge.{0}.views.{0}'.format(module))
+    app.register_blueprint(blueprint)
 
-  return app
+    return app
